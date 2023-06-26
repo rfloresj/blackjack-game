fae99be454469bfdb7f05958f398ad4dea6d3e72
rfloresj <txlinemusic@gmail.com>
2023-06-26 11:44:11 -0600

    display-message-cards-and-sum

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-let firstCard = 3;
-let secondCard = 9;
-
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-console.log(sum);
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
 
+// let sumEl = document.querySelector('#sum-el');  // query = ask, question + selector = CSS selector, ask for a selector
+
+
+function startGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+
+  if (sum < 21) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+// CASH OUT!
