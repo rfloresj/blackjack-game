931479eef88338b29f5aab44dddf3fe1a25f1580
Roberto Flores <txlinemusic@gmail.com>
2023-07-10 09:51:10 -0600

    create-random-numbers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let firstCard = 10;
-let secondCard = 4;
-let cards = [firstCard, secondCard]
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
 
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -8,24 +8,35 @@ let isAlive = true;
 let message = "";
 
 let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById('sum-el');
-let cardsEl = document.getElementById('cards-el');
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 // let sumEl = document.querySelector('#sum-el');  // query = ask, question + selector = CSS selector, ask for a selector
 
-function startGame(){
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+function startGame() {
   renderGame();
 }
 
 function renderGame() {
   cardsEl.textContent = "Cards: ";
-  for(let i =0; i < cards.length; i++ ){
+  for (let i = 0; i < cards.length; i++) {
     cardsEl.textContent += cards[i] + " ";
   }
 
   sumEl.textContent = "Sum: " + sum;
 
-  if (sum < 21) {
+  if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
     message = "You've got Blackjack!";
@@ -38,10 +49,9 @@ function renderGame() {
   messageEl.textContent = message;
 }
 
-function newCard (){
-  let card = 7;
+function newCard() {
+  let card = getRandomCard();
   sum += card;
-  // Push the card to the cards array
   cards.push(card);
   renderGame();
 }
